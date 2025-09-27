c5fe89e07a8da2b60b06a78527e5eb80675e0a12
Ksuxa97 <iksuxa97@gmail.com>
2025-09-27 14:42:08 +0300

    Initial Commit

---
+++ TaskTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TaskTracker
+//
+//  Created by Kseniya Semenova on 27.09.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
